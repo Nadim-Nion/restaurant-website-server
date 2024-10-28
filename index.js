09a382a8c0c4db6ea21063a3f7975d00436b98db
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-10-28 22:17:30 +0600

    delete api for deleting cart data

--- index.js
+++ index.js
@@ -74,6 +74,14 @@ async function run() {
             res.send(result);
         });
 
+        // Delete the cart by its id
+        app.delete('/cartItems/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await cartCollection.deleteOne(query);
+            res.send(result);
+        });
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
